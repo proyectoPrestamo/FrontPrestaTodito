541cd58a0da230f1242927c9eeb7fd7e3a1bf779
Juan3728825 <jmmartinez4817@misena.edu.co>
2023-03-31 07:03:56 -0500

    commit librerias

---
+++ public/js/coordinador.js
@@ -0,0 +1,111 @@
+$(document).ready(function () {
+    $('#registro-coordinador').submit(function (e) {
+      e.preventDefault(); // Evitar que se envíe el formulario de manera convencional
+  
+      var nombre = $('#nombre').val().trim();
+      var apellido = $('#apellido').val().trim();
+      var tipo_documento = $('#tipo_documento').val().trim();
+      var numero_documento = $('#numero_documento').val().trim();
+      var correo_electronico = $('#correo_electronico').val().trim();
+      var telefono = $('#telefono').val().trim();
+      var direccion = $('#direccion').val().trim();
+      var jornada = $('#jornada').val().trim();
+      var genero = $('#genero').val().trim();
+      var contraseña = $('#contraseña').val().trim();
+  
+   // Validar que los campos no estén vacíos, excepto el botón de cancelar
+   var isValid = true;
+   $('#registro-coordinador input:not(.btn-regresar)').each(function() {
+     if ($(this).val().trim() === '') {
+       isValid = false;
+       $(this).addClass('is-invalid');
+       $(this).siblings('.invalid-feedback').show();
+     } else {
+       $(this).removeClass('is-invalid');
+       $(this).siblings('.invalid-feedback').hide();
+     }
+   });
+  
+   if (!isValid) {
+     return;
+   }
+  
+      if (nombre === '' || apellido === '' || tipo_documento === '' || numero_documento === '' || correo_electronico === '' || telefono === '' || direccion === '' || jornada === '' || genero === '' || contraseña === '') {
+        return;
+      }
+  
+      $.ajax({
+        type: "POST",
+        url: "/registrocoordinador",
+        data: { nombre: nombre, apellido: apellido, tipo_documento: tipo_documento, numero_documento: numero_documento, correo_electronico: correo_electronico, telefono: telefono, direccion: direccion, jornada: jornada, genero: genero, contraseña: contraseña },
+        success: function (response) {
+          console.log(response);//verificar si hay error en la ventana emergente
+          // Mostrar alerta de éxito si la respuesta del servidor es exitosa
+          Swal.fire({
+            title: 'Registro exitoso',
+            text: 'Los datos se han registrado correctamente',
+            icon: 'success',
+            confirmButtonColor: '#28A745',
+            confirmButtonText: 'Aceptar',
+            background: '#FFFFFF',
+            customClass: {
+              confirmButton: 'btn btn-success'
+            }
+          });
+  
+          // Vaciar campos del formulario después de enviar los datos
+          $('#registro-coordinador')[0].reset();
+        }
+      });
+    });
+  });
+  
+  $(document).ready(function () {
+    // Esta función se ejecuta cuando el documento HTML ha sido completamente cargado
+  
+    // Evento click para el botón de regresar
+    $('#btn-regresar').click(function (e) {
+      e.preventDefault(); 
+      // Esta función se ejecuta cuando se hace clic en el botón de regresar
+  
+      // Mostrar ventana emergente de SweetAlert
+      Swal.fire({
+        title: '¿Estás seguro?',
+        text: '¿Realmente deseas regresar?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#28a745',
+        cancelButtonColor: '#dc3545',
+        confirmButtonText: 'Sí, regresar',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        // Si se hace clic en el botón de confirmación, redirigir a la vista anterior
+        if (result.isConfirmed) {
+          window.location.href = "/rol";
+                  // Redirige a la ruta /rol
+        } 
+      });
+    });
+  });
+  
+  
+  $(document).ready(function () {
+    // Evento click para el botón de nuevo registro
+    $('#btn-registro').click(function () {
+      // Verificar si todos los campos están llenos
+      if ($('#nombre').val() == '' || $('#apellido').val() == '' || $('#tipo_documento').val() == '' || $('#numero_documento').val() == '' || $('#correo_electronico').val() == '' || $('#telefono').val() == '' || $('#direccion').val() == '' || $('#jornada').val() == '' || $('#genero').val() == '' || $('#contraseña').val() == '') {
+        // Mostrar ventana emergente de SweetAlert
+        Swal.fire({
+          title: 'Error',
+          text: 'Debes llenar todos los campos antes de continuar',
+          icon: 'error',
+          confirmButtonColor: '#dc3545',
+          confirmButtonText: 'OK'
+        });
+        // Detener la ejecución del formulario
+        return false;
+      }
+      // Si todos los campos están llenos, seguir con la ejecución del formulario
+      return true;
+    });
+  });
---
+++ public/js/inventario.js
@@ -0,0 +1,192 @@
+// Selecciona todos los elementos con la clase "delete-icon"
+const deleteIcons = document.querySelectorAll('.delete-icon');
+
+// Itera sobre cada elemento seleccionado y agrega un "listener" al evento "click"
+deleteIcons.forEach(icon => {
+  icon.addEventListener('click', (event) => {
+    // Evita que el evento de clic se propague (para evitar que se haga clic en el elemento padre)
+    event.stopPropagation();
+
+    // Obtiene el elemento "tr" padre del ícono de eliminación (que contiene los datos que deseamos eliminar)
+    const parentTr = event.target.closest('tr');
+
+    // Obtiene el código del producto (el valor del atributo "data-id" del "tr")
+    const productCode = parentTr.getAttribute('data-id');
+
+    // Utiliza SweetAlert para mostrar un mensaje de confirmación y tomar la acción del usuario
+    Swal.fire({
+      title: '¿Estás seguro de que deseas eliminar el producto?',
+      text: `Código de producto: ${productCode}`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#28A745',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Si el usuario hace clic en "Sí, eliminar", elimina el elemento "tr" del DOM
+        parentTr.remove();
+        // Muestra una alerta de éxito utilizando SweetAlert
+        Swal.fire({
+          title: 'Producto eliminado',
+          text: `El producto con código ${productCode} ha sido eliminado correctamente`,
+          icon: 'success',
+          confirmButtonColor: '#28A745'
+        });
+      }
+    })
+  });
+  
+ 
+});
+
+// Seleccionar los elementos HTML necesarios
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const tableRows = document.querySelectorAll('.table tbody tr');
+
+// Función para buscar en la tabla
+function searchTable() {
+  // Obtener el valor de búsqueda
+  const searchValue = searchInput.value.toLowerCase();
+
+  // Verificar si el campo de búsqueda está vacío
+  if (searchValue === '') {
+    // Mostrar alerta si el campo está vacío
+    Swal.fire({
+      icon: 'warning',
+      title: 'Por favor ingresa el nombre o código del insumo a buscar',
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#28A745',
+    });
+    return;
+  }
+
+  // Recorrer todas las filas de la tabla
+  let found = false;
+  tableRows.forEach(row => {
+    // Obtener el texto de la fila y convertirlo a minúsculas
+    const rowText = row.innerText.toLowerCase();
+
+    // Mostrar o ocultar la fila según si coincide con el término de búsqueda
+    if (rowText.includes(searchValue)) {
+      row.style.display = '';
+      found = true;
+    } else {
+      row.style.display = 'none';
+    }
+  });
+
+  // Mostrar alerta si el insumo no ha sido encontrado
+  if (!found) {
+    Swal.fire({
+      icon: 'warning',
+      title: 'El insumo no ha sido encontrado',
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#28A745',
+    });
+  }
+}
+
+// Agregar el evento "keyup" al campo de entrada
+searchInput.addEventListener('keyup', function(event) {
+  // Verificar si se presionó la tecla Enter
+  if (event.keyCode === 13) {
+    searchTable();
+  }
+  
+  // Mostrar todas las filas cuando se borra el campo de búsqueda
+  if (searchInput.value === '') {
+    tableRows.forEach(row => {
+      row.style.display = '';
+    });
+  }
+});
+
+// Agregar el evento "click" al botón de búsqueda
+searchButton.addEventListener('click', function() {
+  searchTable();
+  
+  // Mostrar todas las filas cuando se borra el campo de búsqueda
+  if (searchInput.value === '') {
+    tableRows.forEach(row => {
+      row.style.display = '';
+    });
+  }
+});
+
+$(document).ready(function() {
+  // Función para obtener los datos del producto a editar
+  function obtenerDatosEditar() {
+    let producto = $("#edit-producto").val();
+    let cantidad = $("#edit-cantidad").val();
+    let fecha = $("#edit-fecha").val();
+    let codigo = $("#edit-codigo").val();
+    let estado = $("#edit-estado").val();
+
+    let datos = {
+      producto: producto,
+      cantidad: cantidad,
+      fecha: fecha,
+      codigo: codigo,
+      estado: estado
+    };
+
+    return datos;
+  }
+
+  // Función para guardar los cambios del producto editado
+  function guardarCambios() {
+    let datos = obtenerDatosEditar();
+
+    // Actualizamos los valores de la fila correspondiente
+    let fila = $(`tr[data-id="${idEditar}"]`);
+    fila.find("td:eq(0)").text(datos.producto);
+    fila.find("td:eq(1)").text(datos.cantidad);
+    fila.find("td:eq(2)").text(datos.fecha);
+    fila.find("td:eq(3)").text(datos.codigo);
+    fila.find("td:eq(4)").text(datos.estado + " ");
+
+    // Mostramos los iconos de editar y eliminar
+    fila.find(".edit-icon, .delete-icon").css("visibility", "visible");
+
+    $("#edit-modal").modal("hide");
+  }
+
+  // Función para mostrar el modal y cargar los datos del producto a editar
+  $(".edit-icon").click(function() {
+    idEditar = $(this).closest("tr").data("id");
+    let fila = $(`tr[data-id="${idEditar}"]`);
+
+    // Cargamos los valores del producto a editar en el modal
+    $("#edit-producto").val(fila.find("td:eq(0)").text());
+    $("#edit-cantidad").val(fila.find("td:eq(1)").text());
+    $("#edit-fecha").val(fila.find("td:eq(2)").text());
+    $("#edit-codigo").val(fila.find("td:eq(3)").text());
+    $("#edit-estado").val(fila.find("td:eq(4)").text().trim());
+
+    // Mostramos el modal
+    $("#edit-modal").modal("show");
+  });
+
+  // Función para cancelar la edición del producto
+  $(".cancelar-edicion").click(function() {
+    // Mostramos los iconos de editar y eliminar
+    let fila = $(`tr[data-id="${idEditar}"]`);
+    fila.find(".edit-icon, .delete-icon").css("visibility", "visible");
+
+    $("#edit-modal").modal("hide"); 
+  });
+
+  // Función para guardar los cambios del producto editado al presionar el botón "Guardar"
+  $("#guardar-cambios").click(function() {
+    guardarCambios();
+  });
+
+  // Función para eliminar un producto
+  $(".delete-icon").click(function() {
+    let fila = $(this).closest("tr");
+    fila.remove();
+  });
+});
