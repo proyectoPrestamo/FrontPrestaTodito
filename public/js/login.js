50912c2b88c7ee8e2e77aa6a4ebcfe0843d78c67
Juan3728825 <jmmartinez4817@misena.edu.co>
2023-04-12 10:26:16 -0500

    validaciones y correciones login daniela

--- public/js/login.js
+++ public/js/login.js
@@ -1,57 +1,81 @@
+// Obtiene el elemento del formulario con el ID "login-user"
 const formulario = document.getElementById("login-user");
+
+// Obtiene el botón con el ID "login"
 const botonEnviar = document.getElementById("login");
-const botonOlvidoContraseña = document.getElementById("contraseña-olvidada"); // Agrega el ID del botón "olvido-contraseña" en tu HTML
 
+// Obtiene el botón con el ID "contraseña-olvidada"
+const botonOlvidoContraseña = document.getElementById("contraseña-olvidada");
 
-
-
-// Agregar un evento de escucha al botón de envío
+// Agrega un evento de escucha al botón de envío
 botonEnviar.addEventListener("click", function(event) {
-  // Detener el envío del formulario
-  event.preventDefault();
+// Detiene el envío del formulario
+event.preventDefault();
 
-  // Obtener los valores de los campos del formulario
-  const usuario = document.getElementById("usuario").value;
-  const contraseña = document.getElementById("contraseña").value;
-  const rol = document.getElementById("rol").value;
+// Obtiene los valores de los campos del formulario
+const usuario = document.getElementById("usuario").value;
+const contraseña = document.getElementById("contraseña").value;
+const rol = document.getElementById("rol").value;
 
-  // Verificar si los campos están vacíos
-  if (usuario === "" || contraseña === "" || rol === "") {
-    // Mostrar un mensaje de alerta
-    Swal.fire({
-      title: 'Error',
-      text: 'Debes llenar todos los campos antes de continuar',
-      icon: 'error',
-      confirmButtonColor: '#dc3545',
-      confirmButtonText: 'OK',
-      focusConfirm: false // Evita que el foco se mueva al botón "OK"
-    });
-  } else {
-    location.href="/menu";
-    // Si los campos no están vacíos, enviar el formulario
-    // formulario.submit();
-  }
+// Verifica si los campos están vacíos
+if (usuario === "" || contraseña === "" || rol === "") {
+// Muestra un mensaje de alerta utilizando la librería SweetAlert2
+Swal.fire({
+title: 'Error',
+text: 'Debes llenar todos los campos antes de continuar',
+icon: 'error',
+confirmButtonColor: '#dc3545',
+confirmButtonText: 'OK',
+focusConfirm: false // Evita que el foco se mueva al botón "OK"
+});
+} else {
+// Redirige al usuario según el rol seleccionado
+if (rol === "1") {
+location.href = "/menu-aprendiz";
+} else if (rol === "2") {
+location.href = "/menu-instructor";
+}
+// Si los campos no están vacíos, envía el formulario
+// formulario.submit();
+}
 });
 
-// Agregar un evento de escucha al botón "olvido-contraseña"
+// Agrega un evento de escucha al botón "contraseña-olvidada"
 botonOlvidoContraseña.addEventListener("click", async function(event) {
-  // Detener el envío del formulario
-  event.preventDefault();
+// Detiene el envío del formulario
+event.preventDefault();
 
-  // Mostrar la alerta de ingreso de contraseña utilizando Swal.fire
-  const { value: password } = await Swal.fire({
-    title: 'Ingresa tu correo ',
-    input: 'email',
-    // inputLabel: 'Asegurate de que sea el mismo correo con el cual te encuentras registrado',
-    inputPlaceholder: 'Ingresa tu correo',
-    inputAttributes: {
-      maxlength: 50,
-      autocapitalize: 'off',
-      autocorrect: 'off'
-    }
-  });
+// Muestra la alerta de ingreso de contraseña utilizando SweetAlert2
+const { value: password } = await Swal.fire({
+title: 'Ingresa tu correo ',
+input: 'email',
+// inputLabel: 'Asegurate de que sea el mismo correo con el cual te encuentras registrado',
+inputPlaceholder: 'Ingresa tu correo',
+inputAttributes: {
+maxlength: 50,
+autocapitalize: 'off',
+autocorrect: 'off'
+}
+});
 
-  if (password) {
-    Swal.fire(`Te hemos enviado un correo de recuperacion a: ${password}`);
-  }
+if (password) {
+Swal.fire(`Te hemos enviado un correo de recuperacion a: ${password}`);
+}
+});
+
+// Obtiene los campos del formulario con los ID "usuario" y "contraseña"
+var campos = document.querySelectorAll("#usuario, #contraseña");
+
+// Agrega un evento de escucha a los campos del formulario
+campos.forEach(function(campo) {
+campo.addEventListener("input", function() {
+// Verifica si el campo es el de "usuario"
+if (this.id === "usuario") {
+// Reemplaza cualquier caracter que no sea un número con una cadena vacía
+this.value = this.value.replace(/[^0-9]/g, "");
+} else if (this.id === "contraseña") {
+// Reemplaza cualquier caracter que no sea una letra mayúscula, una letra minúscula, un número o un punto con una cadena vacía
+this.value = this.value.replace(/[^a-zA-Z0-9.]/g, "");
+}
+});
 });
