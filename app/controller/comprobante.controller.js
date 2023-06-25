04f9704d6a32ed41f532ca49a0fb79ba863f350c
Juan3728825 <jmmartinez4817@misena.edu.co>
2023-06-24 23:59:08 -0500

    pdf y excel funcionando...

--- app/controller/comprobante.controller.js
+++ app/controller/comprobante.controller.js
@@ -1,64 +1,75 @@
 import fetch from "node-fetch";
+import Express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import axios from "axios";
+import PDFKit from "pdfkit"
+import fs from "fs"
+import excel from "exceljs"
+
+
 
 const generarPdf = async (req, res) => {
-    try {
-      // Hacer una solicitud GET a la API para obtener la información
-      const response = await axios.get('http://localhost:3000/api/material');
-      const materialData = response.data[0]; // Obtener el primer elemento del arreglo
-  
-      // Crear un nuevo documento PDF
-      const doc = new PDFKit();
-  
-      // Stream el contenido PDF a la respuesta HTTP
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', 'attachment; filename=material.pdf');
-      doc.pipe(res);
-  
-      // Agregar el logo del proyecto
-      const imagePath = path.join(__dirname, 'public', 'img', 'logoSena.png');
-      const logoWidth = 200;
-      const logoHeight = 100;
-  
-      const pageWidth = doc.page.width;
-      const pageHeight = doc.page.height;
-  
-      const logoX = (pageWidth - logoWidth) / 2;
-      const logoY = (pageHeight - logoHeight) / 2;
-  
-      doc.image(imagePath, logoX, logoY, { width: logoWidth, height: logoHeight });
-  
-      // Agregar el encabezado
-      doc.fontSize(24).text('Registro de materiales', { align: 'center' });
-  
-      // Agregar contenido al PDF
-      doc.moveDown(2); // Espacio adicional entre el encabezado y la información del material
-      doc.fontSize(18).text('Información del material', { align: 'center' });
-      materialData.forEach((material) => {
-        doc.fontSize(12).text(`ID: ${material.ID_MATERIAL}`);
-        doc.fontSize(12).text(`Nombre del material: ${material.NOMBRE}`);
-        doc.fontSize(12).text(`Tipo de material: ${material.TIPO}`);
-        doc.fontSize(12).text(`Estado del material: ${material.ESTADO}`);
-        doc.fontSize(12).text(`Cantidad del material: ${material.CANTIDAD}`);
-        doc.fontSize(12).text(`Color del material: ${material.COLOR}`);
-        doc.fontSize(12).text(`Medida del material: ${material.MEDIDA}`);
-  
-        doc.moveDown(); // Agrega un espacio entre cada material
-      });
-  
-      // Agregar el pie de página
-      const generador = 'prestaTodito';
-      const fechaImpresion = new Date().toLocaleString();
-      doc.fontSize(10).text(`Generado por: ${generador}`);
-      doc.fontSize(10).text(`Fecha y hora de impresión: ${fechaImpresion}`, { align: 'right' });
-  
-      // Finalizar el PDF
-      doc.end();
-    } catch (error) {
-      // Manejar errores de solicitud o cualquier otro error
-      console.error(error);
-      res.status(500).send('Error al generar el PDF');
-    }
-  };
+  try {
+    // Hacer una solicitud GET a la API para obtener la información
+    const response = await axios.get('http://localhost:3000/api/material');
+    const materialData = response.data[0]; // Obtener el primer elemento del arreglo
+
+    // Crear un nuevo documento PDF
+    const doc = new PDFKit();
+
+    // Stream el contenido PDF a la respuesta HTTP
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=material.pdf');
+    doc.pipe(res);
+
+    // Agregar el logo del proyecto
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const logoHeight = 100;
+    const logoWidth = 200;
+    const imagePath = 'D:/Users/lenovo/Documents/FRONT Y BACK TODITO/FRONT-TODITO/public/img/logoSena.png';
+    //const imagePath = path.join(__dirname, '..', 'public', 'img', 'logoSena.png');
+
+    const pageWidth = doc.page.width;
+    const pageHeight = doc.page.height;
+
+    const logoX = (pageWidth - logoWidth) / 2;
+    const logoY = (pageHeight - logoHeight) / 2;
+
+    doc.image(imagePath, logoX, logoY, { width: logoWidth, height: logoHeight });
+
+    // Agregar el encabezado
+    doc.fontSize(24).text('Registro de materiales', { align: 'center' });
+
+    // Agregar contenido al PDF
+    doc.moveDown(2); // Espacio adicional entre el encabezado y la información del material
+    doc.fontSize(18).text('Información del material', { align: 'center' });
+    materialData.forEach((material) => {
+      doc.fontSize(12).text(`ID: ${material.ID_MATERIAL}`);
+      doc.fontSize(12).text(`Nombre del material: ${material.NOMBRE}`);
+      doc.fontSize(12).text(`Tipo de material: ${material.TIPO}`);
+      doc.fontSize(12).text(`Color del material: ${material.COLOR}`);
+      doc.fontSize(12).text(`Medida del material: ${material.MEDIDAS}`);
+
+      doc.moveDown(); // Agrega un espacio entre cada material
+    });
+
+    // Agregar el pie de página
+    const generador = 'prestaTodito';
+    const fechaImpresion = new Date().toLocaleString();
+    doc.fontSize(10).text(`Generado por: ${generador}`);
+    doc.fontSize(10).text(`Fecha y hora de impresión: ${fechaImpresion}`, { align: 'right' });
+
+    // Finalizar el PDF
+    doc.end();
+  } catch (error) {
+    // Manejar errores de solicitud o cualquier otro error
+    console.error(error);
+    res.status(500).send('Error al generar el PDF');
+  }
+};
+
   
   const generarexcel = async (req, res) => {
     try {
@@ -119,7 +130,8 @@ const generarPdf = async (req, res) => {
       res.status(500).send('Error al generar el archivo Excel');
     }
   };
+  
   export const comprobanteController = {
-    generarPdf, generarexcel 
-};
-
+    generarPdf,
+    generarexcel
+  };
--- app/routes/comprobante.routes.js
+++ app/routes/comprobante.routes.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import middle from "../middleware/middleware.js";
 import { comprobanteController } from "../controller/comprobante.controller.js";
+
 const comprobante = Router();
 
-comprobante.get('/generarPdf',middle,comprobanteController.generarPdf)
-
-comprobante.get('/generarexcel',middle,comprobanteController.generarexcel)
-
+comprobante.post('/generarpdf', middle, comprobanteController.generarPdf);
+comprobante.post('/generarexcel', middle, comprobanteController.generarexcel);
 
 export default comprobante;
